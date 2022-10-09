91c38fc64a8176badee21d2c2b336ff8807277a2
ederjbbb <ederjb@icloud.com>
2022-10-09 20:46:42 +0100

    modal

--- src/components/Heaader/index.tsx
+++ src/components/Heaader/index.tsx
@@ -1,12 +1,20 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from './../../assets/dtmoney-logo.svg';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import logoImg from './../../assets/dtmoney-logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModel'
 
-export function Header () {
-    return <HeaderContainer>
-                <HeaderContent>
-                    <img src={logoImg} alt="" />
-
-                    <NewTransactionButton>Nova Transacao</NewTransactionButton>
-                </HeaderContent>  
-            </HeaderContainer>
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <NewTransactionModal />
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transacao</NewTransactionButton>
+          </Dialog.Trigger>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
